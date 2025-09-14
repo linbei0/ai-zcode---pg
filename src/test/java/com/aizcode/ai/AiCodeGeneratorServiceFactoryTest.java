eca88e9941b9a9e86345cb8ee6e7f0e37ba759df
Jgit <WY163stxh@163.com>
2025-09-14 13:58:08 +0800

    feat(core): 实现 AI 代码生成核心功能
    - 新增 AiCodeGeneratorFacade 类作为代码生成的外观类
    - 添加 AiCodeGeneratorService 接口和实现类，用于生成 HTML 和多文件代码- 实现 CodeFileSaver 类用于保存生成的代码到文件- 添加 CodeParser 类用于解析生成的代码内容
    - 新增相关测试类进行功能验证
    - 更新 .gitignore 和 application.yml 文件- 添加 langchain4j 相关依赖

---
+++ src/test/java/com/aizcode/ai/AiCodeGeneratorServiceFactoryTest.java
@@ -0,0 +1,53 @@
+package com.aizcode.ai;
+
+import com.aizcode.ai.model.HtmlCodeResult;
+import com.aizcode.ai.model.MultiFileCodeResult;
+import com.aizcode.core.AiCodeGeneratorFacade;
+import com.aizcode.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+import java.io.File;
+import java.util.List;
+
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个程序员鱼皮的工作记录小工具,不超过50行");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个程序员鱼皮的留言板，不超过50行");
+        Assertions.assertNotNull(multiFileCode);
+    }
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("任务记录网站,不超过30行", CodeGenTypeEnum.MULTI_FILE);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+}
