675e2989b8156c0d3c1d2a0a28a822a3ceec1a3e
Jgit <WY163stxh@163.com>
2025-09-29 14:42:40 +0800

    feat(microservice): 集成Dubbo实现微服务间RPC通信

--- ai-zcode-microservice/ai-zcode-user/src/main/java/com/aizcodeuser/AiZcodeUserApplication.java
+++ ai-zcode-microservice/ai-zcode-user/src/main/java/com/aizcodeuser/AiZcodeUserApplication.java
@@ -1,5 +1,6 @@
 package com.aizcodeuser;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.aizcodeuser.mapper")
+@EnableDubbo
 //@ComponentScan("com")
 public class AiZcodeUserApplication {
     public static void main(String[] args) {
---
+++ ai-zcode-microservice/ai-zcode-user/src/main/java/com/aizcodeuser/service/impl/InnerUserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.aizcodeuser.service.impl;
+
+import com.aizcode.innerservice.InnerUserService;
+import com.aizcode.model.entity.User;
+import com.aizcode.model.vo.UserVO;
+import com.aizcodeuser.service.UserService;
+import jakarta.annotation.Resource;
+import org.apache.dubbo.config.annotation.DubboService;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@DubboService
+public class InnerUserServiceImpl implements InnerUserService {
+
+    @Resource
+    private UserService userService;
+
+    @Override
+    public List<User> listByIds(Collection<? extends Serializable> ids) {
+        return userService.listByIds(ids);
+    }
+
+    @Override
+    public User getById(Serializable id) {
+        return userService.getById(id);
+    }
+
+    @Override
+    public UserVO getUserVO(User user) {
+        return userService.getUserVO(user);
+    }
+}
+
